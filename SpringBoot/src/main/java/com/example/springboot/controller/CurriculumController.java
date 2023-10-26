bb2409feaf509888e862a17b702e507c18847c56
Sybe Aerts <s1144856@student.hsleiden.nl>
2023-10-26 11:29:29 +0200

    FEAT: requestmapping toegevoegd aan curriculum, zodat je de onderliggende tabellen kan bereiken

--- SpringBoot/src/main/java/com/example/springboot/controller/CurriculumController.java
+++ SpringBoot/src/main/java/com/example/springboot/controller/CurriculumController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/curriculum")
 public class CurriculumController {
     private final CurriculumDao curriculumDao;
     @Autowired
