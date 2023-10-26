5ea055816ed0ce14be02cb383b7477bb06e813c6
Sybe Aerts <s1144856@student.hsleiden.nl>
2023-10-26 16:05:21 +0200

    Revert "FEAT: requestmapping toegevoegd aan curriculum, zodat je de onderliggende tabellen kan bereiken"
    This reverts commit bb2409feaf509888e862a17b702e507c18847c56.

--- SpringBoot/src/main/java/com/example/springboot/controller/CurriculumController.java
+++ SpringBoot/src/main/java/com/example/springboot/controller/CurriculumController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/curriculum")
 public class CurriculumController {
     private final CurriculumDao curriculumDao;
     @Autowired
