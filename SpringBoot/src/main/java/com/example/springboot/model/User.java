28af6008d254575ff124f24ecc542040a6aff647
Jeremy Utomo <s1143873@student.hsleiden.nl>
2023-10-26 16:14:31 +0200

    FEAT: Branch versie4

--- SpringBoot/src/main/java/com/example/springboot/model/User.java
+++ SpringBoot/src/main/java/com/example/springboot/model/User.java
@@ -16,6 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
+//
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
