8f6f1684274857294067b119c624a67c1df636e1
Hicham <s1143822@student.hsleiden.nl>
2024-01-28 13:04:47 +0100

    FIX: UserExercises now work correctly

--- SpringBoot/src/main/java/com/example/springboot/repository/UserExercisesRepository.java
+++ SpringBoot/src/main/java/com/example/springboot/repository/UserExercisesRepository.java
@@ -12,8 +12,8 @@ import java.util.List;
 public interface UserExercisesRepository extends JpaRepository<UserExercises, Integer> {
 
     @Query(value = "SELECT e.name, ue.exerciseToDo, ue.exerciseDone, ue.lastDone FROM UserExercises ue " +
-            "JOIN ue.exercise e " +
-            "JOIN ue.user u " +
+            "JOIN ue.exercise e ON ue.exercise = e " +
+            "JOIN ue.user u ON ue.user = u " +
             "JOIN CurriculumExercises ce ON ce.exercise = e " +
             "JOIN Curriculum c ON c = ce.curriculum " +
             "WHERE u.username = :username " +
--- SpringBoot/src/main/java/com/example/springboot/repository/UserRepository.java
+++ SpringBoot/src/main/java/com/example/springboot/repository/UserRepository.java
@@ -2,14 +2,16 @@ package com.example.springboot.repository;
 
 import com.example.springboot.model.User;
 import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, UUID>{
     Optional<User> findByUsername(String username);
 
-    Optional<User> findUserByID(Long id);
+    Optional<User> findUserByID(UUID id);
 
     Boolean existsByUsername(String username);
 }
