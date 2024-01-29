0c8eedf6d48217d70d8274a4eb802cb1b7671ddc
Sybe Aerts <s1144856@student.hsleiden.nl>
2024-01-29 10:43:17 +0100

    FIX: annotation werkend krijgen

--- SpringBoot/src/main/java/com/example/springboot/controller/AuthenticationController.java
+++ SpringBoot/src/main/java/com/example/springboot/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.example.springboot.dao.AuthenticationDao;
 import com.example.springboot.model.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,7 @@ public class AuthenticationController {
     private final AuthenticationDao service;
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody User request
+            @Valid @RequestBody User request
     ){
         return ResponseEntity.ok(service.register(request));
     }
